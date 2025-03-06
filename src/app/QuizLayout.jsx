1119b684c86c63c7daf8195fdf304ae93bd5029c
vinaykanth97 <vinaykennedy@gmail.com>
2025-03-07 00:04:21 +0530

    Updated somethings related to remove

--- src/app/QuizLayout.jsx
+++ src/app/QuizLayout.jsx
@@ -9,26 +9,36 @@ import { useRouter } from 'next/navigation'
 export const QuizLayout = ({ quizDatas }) => {
     const router = useRouter()
     const [checkboxValue, setcheckboxValue] = QuizHook([])
+    const [checkErrors, setCheckErrors] = useState([])
     const SelectHandler = (e, singleChoice) => {
         let { name, value, checked } = e.target
+
         if (checked) {
             setcheckboxValue((prev) => {
                 return {
                     ...prev,
-                    [name]: !singleChoice ? [value, ...(prev?.[name] !== undefined ? prev?.[name] : "")] : value
+                    [name]: !singleChoice ? [...(prev?.[name] !== undefined ? prev?.[name] : ""), value] : value,
+                    containsValue: true
                 }
             })
         } else {
+
             setcheckboxValue((prev) => {
+
                 return {
                     ...prev,
-                    [name]: !singleChoice ? [value, ...(prev?.[name] !== undefined ? prev?.[name]?.filter(nm => nm === value) : "")] : value
+                    [name]: !singleChoice ?
+                        [...(prev?.[name] !== undefined ? prev?.[name].filter(nm => nm !== value) : "")]
+                        : value,
+                    containsValue: prev?.[name].length <= 1 ? false : true
                 }
             })
         }
-
+        // console.log(checkboxValue)
     }
     useEffect(() => {
+
+
         console.log(checkboxValue);
 
     }, [checkboxValue])
@@ -36,7 +46,7 @@ export const QuizLayout = ({ quizDatas }) => {
     const HandleQuizSubmit = (e) => {
         e.preventDefault()
         let convertObj = new URLSearchParams(checkboxValue).toString()
-        console.log(convertObj);
+
 
         router.push(
             `/results?${convertObj}`,
@@ -46,25 +56,29 @@ export const QuizLayout = ({ quizDatas }) => {
     return (
         <form onSubmit={HandleQuizSubmit}>
             {quizDatas?.map((datas, i) => {
+                console.log(checkboxValue);
+                
                 return (
                     <div key={i}>
                         <h2>{datas.question}</h2>
                         <div className='choiceswrap'>
                             {datas.choices?.map((choice, j) => {
-
                                 return (
                                     datas.singleChoice ?
+
                                         <div className='inputWrap' key={j}>
-                                            <input type="radio" name={datas.uniquename} id={choice.name} value={choice.text} onInput={(e) => SelectHandler(e, datas.singleChoice)} />
+                                            <input type="radio" name={datas.uniquename} id={choice.name} value={choice.text} onChange={(e) => SelectHandler(e, datas.singleChoice)} />
                                             <label htmlFor={choice.name}>{choice.text}</label>
                                         </div>
+
                                         : <div className='inputWrap' key={j}>
-                                            <input type="checkbox" name={datas.uniquename} id={choice.name} value={choice.text} onInput={(e) => SelectHandler(e, datas.singleChoice)} />
+                                            <input type="checkbox" name={datas.uniquename} id={choice.name} value={choice.text} onChange={(e) => SelectHandler(e, datas.singleChoice)} />
                                             <label htmlFor={choice.name}>{choice.text}</label>
                                         </div>
+
                                 )
                             })}
-
+                            <p className="">{checkboxValue?.[datas.uniquename]?.containsValue?.toString()}</p>
                         </div>
                     </div>
                 )
